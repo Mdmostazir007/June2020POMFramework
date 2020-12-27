5d119e2943d62c5fd7cd15243f7cfdf2be5240e9
Mdmostazir <mdmostazir@outlook.com>
2020-12-26 19:04:16 -0500

    add Extent, allure and Tesult Listener

--- src/main/java/com/qa/hubspot/utils/Constants.java
+++ src/main/java/com/qa/hubspot/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants{
    //***************************Constants Name***************************************
 	
 	public static final String LOGIN_PAGE_TITLE = "Login - My Store";
-	public static final String  HOME_PAGE_TITLE = "My account - My Store123";
+	public static final String  HOME_PAGE_TITLE = "My account - My Store";
 	public static final String  HOME_PAGE_HEADER = "MY ACCOUNT";
 	public static final String  CONTACTS_PAGE_TITLE = "Contact us - My Store";
 	public static final String  CONTACTS_PAGE_HEADER = "CUSTOMER SERVICE - CONTACT US";
